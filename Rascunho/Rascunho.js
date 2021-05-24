f3dfa847418886766da8aaf6cca8d58fa35e09b1
gabiazevedo <gabiazevedoms@gmail.com>
2021-05-24 10:31:26 -0300

    Adiciona pasta para guardar todo o conteúdo do módulo de fundamentos

--- Rascunho/Rascunho.js
+++ Rascunho/Rascunho.js
@@ -1,5 +1,18 @@
-function greeting() {
-greeting.lang('English');
-console.log(lang);
-}
-greeting(lang);
+const persons = [
+{ firstName: 'Gabriela', lastName: 'Azevedo' },
+
+{ firstName: 'Ana', lastName: 'Giorgiani'}
+]
+
+// 1 - Retornar um objeto com os pares de chave/valor do objeto persons
+
+const newObj = persons.reduce((acc, curr) => {
+acc[curr.firstName] = curr.lastName
+return acc;
+}, {});
+console.log(newObj);
+
+// 2 - Retornar um array com os valores das propriedades lastName
+
+const arrayLastName = persons.map((lastName) => lastName.lastName);
+console.log(arrayLastName);
