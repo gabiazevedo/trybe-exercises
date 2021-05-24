c1f433a70f8627c3c0517f39ce31fc1c740cf597
gabiazevedo <gabiazevedoms@gmail.com>
2021-05-24 16:12:26 -0300

    Adicina Hello World - React

---
+++ Bloco_11/hello-world-react/src/Component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const name = 'Gabriela Azevedo';
+class Component extends React.Component {
+  render() {
+    return <div>
+    <h1>{name}</h1>
+    <p>Me chamo {name}, tenho 32 anos, sou confeiteira, apaixonada por tecnologia e estou super feliz de chegar ao Front End - React</p>
+    </div>
+  }
+}
+
+export default Component 
