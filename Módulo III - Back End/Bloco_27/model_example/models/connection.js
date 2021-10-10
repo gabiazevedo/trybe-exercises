7a010cf39bc07f4872d00d69d4dc0beff7334529
gabiazevedo <gabiazevedoms@gmail.com>
2021-10-10 14:25:20 -0300

    Adiciona exercícios de fixação dia 27.1 - Integração Node + MongoDB

--- Módulo III - Back End/Bloco_27/model_example/models/connection.js
+++ Módulo III - Back End/Bloco_27/model_example/models/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'gabiazevedo',
-  password: 'Ektopak110910#',
+  password: '',
   database: 'model_example' });
 
 module.exports = connection;
