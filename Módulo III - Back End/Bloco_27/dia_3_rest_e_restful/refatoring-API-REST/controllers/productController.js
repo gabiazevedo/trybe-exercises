f9195215a1fa81419f8cefc85b5bc1f081bc7f09
gabiazevedo <gabiazevedoms@gmail.com>
2021-10-16 17:49:12 -0300

    Sobe exercícios do dia 27.3

--- Módulo III - Back End/Bloco_27/dia_3_rest_e_restful/refatoring-API-REST/controllers/productController.js
+++ Módulo III - Back End/Bloco_27/dia_3_rest_e_restful/refatoring-API-REST/controllers/productController.js
@@ -1,40 +1,51 @@
 const express = require('express');
 const ProductModel = require('../models/productModel');
-
 const router = express.Router();
 
-router.get('/list-products', async (req, res, next) => {
+router.get('/', async (_req, res) => {
   const products = await ProductModel.getAll();
-
-  res.send(products);
+  if (!products) {
+    res.status(404).json({ message: 'Not found'});
+  };
+  res.status(200).json(products);
+  console.log('estou no GET');
 });
 
-router.get('/get-by-id/:id', async (req, res, next) => {
-  const product = await ProductModel.getById(req.params.id);
-
-  res.send(product);
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await ProductModel.getById(id);
+  if (!product) {
+    res.status(404).json({ message: 'Product not found' })
+  };
+  res.status(200).json(product);
 });
 
-router.post('/add-user', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
-
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  if (!newProduct) {
+    res.status(400).json({ message: 'Bad Request' });
+  };
+  res.status(201).json(newProduct);
 });
 
-router.post('/delete-user/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
-
-  res.send(products);
-});
-
-router.post('/update-user/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
   const { name, brand } = req.body;
+  const products = await ProductModel.update(id, name, brand);
+  
+  if (!products || products === null) {
+    res.status(400).json({ message: 'Bad Request' });
+  };
+  res.status(200).json(products);
+});
 
-  const products = await ProductModel.update(req.params.id, name, brand);
-
-  res.send(products);
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const deleteProduct = await ProductModel.exclude(id);
+  
+  res.status(204).json(deleteProduct);
 });
 
 module.exports = router;
