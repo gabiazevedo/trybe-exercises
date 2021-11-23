ccd35d16bd7b83789dd962269e953b6d1c391d5e
brunacamposxx <92brunacampos@gmail.com>
2021-11-23 15:06:46 -0300

    Sobre setup exercícios

--- Módulo III - Back End/Bloco_27/dia_1_camada_de_modelo/model_example/models/connection.js
+++ Módulo III - Back End/Bloco_27/dia_1_camada_de_modelo/model_example/models/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'gabiazevedo',
-  password: '',
+  password: 'Ektopak110910#',
   database: 'model_example' });
 
 module.exports = connection;
