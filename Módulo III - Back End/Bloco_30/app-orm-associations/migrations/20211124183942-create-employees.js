eb590d1a7b52dfc9fec5b1efc8a48e07ecba9b99
gabiazevedo <gabiazevedoms@gmail.com>
2021-11-24 17:59:00 -0300

    chore: Adiciona configurações iniciais da aplicação

---
+++ Módulo III - Back End/Bloco_30/app-orm-associations/migrations/20211124183942-create-employees.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'first_name',
+      },
+      age: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Employees');
+  },
+};
